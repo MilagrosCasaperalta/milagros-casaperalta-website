4a89c35dd22ec2301b2e7f14608dcfcb1bc8ace1
MilagrosCasaperalta <milagrosadriana.casaperalta@mail.polimi.it>
2025-06-12 14:05:51 +0200

    Change projects.jsx

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -19,7 +19,7 @@ function Projects() {
                         <h3>{project.title}</h3>
                         <p>{project.description}</p>
                         <a href={project.link} target="_blank" rel="noopener noreferrer">
-                            Visit
+                            Have a look here
                         </a>
                     </li>
                 ))}
